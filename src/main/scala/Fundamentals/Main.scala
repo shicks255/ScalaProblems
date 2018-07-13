3f0efb8b977bd9022ba89b58cf4bcfc4cdd07138
shicks <shicks255@yahoo.com>
2018-07-12 21:02:09 -0400

    some more scala fundamental practice

--- src/main/scala/Fundamentals/Main.scala
+++ src/main/scala/Fundamentals/Main.scala
@@ -1,32 +1,45 @@
 package Fundamentals
 
 object Main extends App{
-
   Utilities.printSomeStrings(1)
-
   Utilities.printFirstLetter("steven")
   Utilities.printLastLetter("steven")
   Utilities.printStringMultiply("steven")
-
+  Utilities.oldEnoughToDrink(21)
+  Utilities.oldEnoughToDrink(2)
 }
 
 object Utilities {
 
-  def printSomeStrings(number: Int) = {
-    println(10 min 15)
+//  Conditional
+  def oldEnoughToDrink(age: Int) =
+  {
+    if (age >= 21)
+      println(age + " is old enough to drink.")
+    else
+      println(age + " is not old enough to drink")
   }
 
+//  For Loops
+  def printSomeStrings(number: Int) = {
+    for (i <- 1 to number)
+      println("Line# " + i)
+  }
+
+//  String Manipulation
   def printFirstLetter(word: String) = {
     println(word.head)
     println(word.take(1)) //another way to do it
   }
 
+//  String Manipulation
   def printLastLetter(word: String) = {
 //    println(word(word.length - 1))
     //better way to do it
     println(word.takeRight(1))
   }
 
+//  Multiply string by int
   def printStringMultiply(word: String) = {
     println(word * 3)
   }
