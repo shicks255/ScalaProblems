fe28524a39de3aaed0e5ea392a941c4fde2b54bc
shicks <shicks255@yahoo.com>
2018-07-20 11:44:39 -0400

    setting up new problem, are they the same

---
+++ src/main/scala/CodeWars/AreTheyTheSame/Main.scala
@@ -0,0 +1,18 @@
+//https://www.codewars.com/kata/are-they-the-same/train/scala
+
+package CodeWars.AreTheyTheSame
+
+object Main extends App{
+
+  val seq1 = Seq(121, 144, 19, 161, 19, 144, 19, 11)
+  val seq2 = Seq(11*11, 121*121, 144*144, 19*19, 161*161, 19*19, 144*144, 19*19)
+
+  println(comp(seq1, seq2)) // should return true;
+
+  def comp(seq1: Seq[Int], seq2: Seq[Int]): Boolean = {
+
+    
+
+    true
+  }
+}
