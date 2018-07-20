814cff2f628a3ee53d3740d6cd125461e637987c
shicks <shicks255@yahoo.com>
2018-07-19 22:55:56 -0400

    this is ugly, and not very 'functional', but it works

--- src/main/scala/CodeWars/AllInclusive/Main.scala
+++ src/main/scala/CodeWars/AllInclusive/Main.scala
@@ -1,19 +1,28 @@
 package CodeWars.AllInclusive
 
+import scala.collection.mutable.ArrayBuffer
+
 object Main extends App{
-  var a = List("bsjq", "qbsj", "sjqb", "twZNsslC", "jqbs")
-  containAllRots("bsjq", a)
+  var arr = List("bsjq", "qbsj", "sjqb", "twZNsslC", "jqbs")
+  print(containAllRots("bsjq", arr))
 
-  def containAllRots(string: String, arr: List[String]): Boolean = {
+  def containAllRots(strng: String, arr: List[String]): Boolean = {
 
-    var array = string.toCharArray
+    var charArray = strng.toCharArray
+    var rots = ArrayBuffer[String]()
 
-    val thing = for (i <- 0 until string.length) yield {
-
-      val temp = arr(i)
-      var newArray = array
-      newArray(i) =
+     var arrayOfRots = for (_ <- 0 until strng.length)
+     {
+       var temp = charArray(0)
+       for (x <- 1 until strng.length)
+         charArray(x-1) = charArray(x)
+       charArray(strng.length - 1) = temp
+       rots += charArray.mkString("")
     }
+
+    for (rot <- rots)
+        if (!arr.contains(rot))
+          return false;
 
     true
   }
