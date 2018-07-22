d986173abbf5bb6eee33006f50dee7a8910f5e46
shicks <shicks255@yahoo.com>
2018-07-22 13:31:36 -0400

    settings up new CodeWars

---
+++ src/main/scala/CodeWars/TransformToPrime/Main.scala
@@ -0,0 +1,18 @@
+package CodeWars.TransformToPrime
+
+object Main extends App{
+
+  val test1 = Seq(1,2,3) // 1
+  val test2 = Seq(5,2) // 0
+  val test3 = Seq(2,12,8,4,6) // 5
+
+  def minumumNumber(numbers: Seq[Int]): Int = {
+
+    //Prime number is only divisible by 1 and itself
+    //therefor, nothing even first of all.
+
+
+    1
+  }
+
+}
