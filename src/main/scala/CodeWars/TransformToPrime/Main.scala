517130ba58e17550a91bfb7aacace422f674807a
shicks <shicks255@yahoo.com>
2018-07-22 13:33:07 -0400

    working on this problem

--- src/main/scala/CodeWars/TransformToPrime/Main.scala
+++ src/main/scala/CodeWars/TransformToPrime/Main.scala
@@ -1,3 +1,5 @@
+//https://www.codewars.com/kata/5a946d9fba1bb5135100007c/train/scala
+
 package CodeWars.TransformToPrime
 
 object Main extends App{
@@ -6,11 +8,16 @@ object Main extends App{
   val test2 = Seq(5,2) // 0
   val test3 = Seq(2,12,8,4,6) // 5
 
+  minumumNumber(test3)
+
   def minumumNumber(numbers: Seq[Int]): Int = {
 
     //Prime number is only divisible by 1 and itself
     //therefor, nothing even first of all.
 
+    var currentSum = numbers.sum
+
+    println(currentSum)
 
     1
   }
