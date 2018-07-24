77ade7d39a13ecdbd9603ddb9a3138902b04e57f
shicks <shicks255@yahoo.com>
2018-07-24 18:59:01 -0400

    this is done.  solution is pretty good, but verbose, and not "functional"

--- src/main/scala/CodeWars/TransformToPrime/Main.scala
+++ src/main/scala/CodeWars/TransformToPrime/Main.scala
@@ -8,18 +8,42 @@ object Main extends App{
   val test2 = Seq(5,2) // 0
   val test3 = Seq(2,12,8,4,6) // 5
 
-  minumumNumber(test3)
-
-  def minumumNumber(numbers: Seq[Int]): Int = {
+  println(minumumNumber(test3))
 
     //Prime number is only divisible by 1 and itself
     //therefor, nothing even first of all.
 
+  def minumumNumber(numbers: Seq[Int]): Int = {
     var currentSum = numbers.sum
 
-    println(currentSum)
+    if (isPrime(currentSum))
+      return 0;
 
-    1
+    var numberToAdd = 1;
+    val answer = findPrime(currentSum, numberToAdd)
+    answer-currentSum
+  }
+
+  def findPrime(currentSum: Int, numberToAdd: Int): Int = {
+    var newNumber = currentSum + numberToAdd;
+
+    if (isPrime(newNumber))
+      return newNumber
+
+    if (newNumber % 2 == 0)
+      findPrime(newNumber, 1)
+    else
+      findPrime(newNumber, 2)
+  }
+
+  def isPrime(number : Int): Boolean = {
+
+    for (i <- 2 until number)
+      {
+        if (number % i == 0)
+          return false;
+      }
+    true
   }
 
 }
