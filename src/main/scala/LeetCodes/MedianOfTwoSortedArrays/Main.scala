77c46a132ab99d4c393039791ce6384443a7f761
shicks <shicks255@yahoo.com>
2018-08-01 21:26:30 -0400

    going to try some more advanced scala collections stuff

--- src/main/scala/LeetCodes/MedianOfTwoSortedArrays/Main.scala
+++ src/main/scala/LeetCodes/MedianOfTwoSortedArrays/Main.scala
@@ -1,11 +1,41 @@
+//https://leetcode.com/problems/median-of-two-sorted-arrays/description/
+/**
+  * There are two sorted arrays nums1 and nums2 of size m and n respectively.
+  *
+  * Find the median of the two sorted arrays.
+  * The overall run time complexity should be O(log (m+n)).
+  *
+  * You may assume nums1 and nums2 cannot be both empty.
+  *
+  *
+  *
+  * Example 1:
+  *
+  * nums1 = [1, 3]
+  * nums2 = [2]
+  *
+  * The median is 2.0
+  * Example 2:
+  *
+  * nums1 = [1, 2]
+  * nums2 = [3, 4]
+  *
+  * The median is (2 + 3)/2 = 2.5
+  */
 package LeetCodes.MedianOfTwoSortedArrays
 
 object Main extends App{
 
+  findMedianSortedArrays(Array(1,2,3), Array(2,4,6))
 
-//  def findMedianSortedArrays(nums1: Array[Int], nums2: Array[Int]): Double = {
-//
-//
-//    2
-//  }
+  def findMedianSortedArrays(nums1: Array[Int], nums2: Array[Int]): Double = {
+
+    val nums3 = nums1 ++ nums2
+
+    val newSeq = nums3.toSeq.sorted
+
+
+
+    2
+  }
 }
