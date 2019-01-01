ef4fdadaa371b524af3357a4f66b058e655ffbef
shicks <shicks255@yahoo.com>
2019-01-01 17:46:34 -0500

    playing some more with these things

--- src/main/scala/Fundamentals/ObjectsAndClasses/Cat.scala
+++ src/main/scala/Fundamentals/ObjectsAndClasses/Cat.scala
@@ -1,19 +1,39 @@
 package Fundamentals.ObjectsAndClasses
 
+import java.time.LocalDate
+
 import scala.collection.mutable.ListBuffer
 
-class Cat(name: String, age: Int) {
-
-  def print(): String = name + " is " + age + " years old"
+class Cat(n: String, dob: LocalDate, g: String) {
+  private val name: String = n
+  private val dateOfBirth: LocalDate = dob
+  private val gender: String = g
+  var dateOfDeath: LocalDate = null
+  def print(): String = name + " is " + getAge() + " years old, and is a " + gender
   override def toString(): String = {
-    name + " - " + age
+    name + " - " + getAge() + " : " + gender
   }
+
+  def getAge(): Int = {
+    val today = if (dateOfDeath != null) dateOfDeath else LocalDate.now()
+    val age: Int = today.getYear - dob.getYear
+    age
+  }
+
+  def isDead(): Boolean = dateOfDeath != null
 }
 
 
 object Cat {
   var adoptedCats = new ListBuffer[Cat]
 
-  def adoptACat(cat: Cat): Unit = adoptedCats += cat
-  def printAdoptedCats(): Unit = println(adoptedCats.mkString(","))
+  def adoptACat(cat: Cat): Unit =
+  {
+      adoptedCats += cat
+      println(cat.name + " added to list of adopted cats")
+  }
+  def printAdoptedCats(): Unit = println(adoptedCats.mkString(", "))
+  def catsAlive(): List[Cat] = adoptedCats.filter(p => p.isDead() == false).toList
+  def catsDead(): List[Cat] = adoptedCats.filter(p => p.isDead()).toList
+
 }
--- src/main/scala/Fundamentals/ObjectsAndClasses/Runner.scala
+++ src/main/scala/Fundamentals/ObjectsAndClasses/Runner.scala
@@ -1,13 +1,22 @@
 package Fundamentals.ObjectsAndClasses
 
+import java.time.LocalDate
+
 object Runner {
   def main(args: Array[String]) = {
-    val boo = new Cat("Boo Cat", 11)
+    val boo = new Cat("Boo Cat", LocalDate.of(2007, 6, 23), "female")
     Cat.adoptACat(boo)
 
-    val cruiser = new Cat("Cruiser", 12)
+    val cruiser = new Cat("Cruiser", LocalDate.of(2001, 7, 15), "male")
+    cruiser.dateOfDeath = LocalDate.of(2012, 10, 5)
     Cat.adoptACat(cruiser)
 
     Cat.printAdoptedCats()
+
+    println("Cats still alive:")
+    println(Cat.catsAlive().mkString(", "))
+
+    println("Cats that are dead")
+    println(Cat.catsDead().mkString(", "))
   }
 }
