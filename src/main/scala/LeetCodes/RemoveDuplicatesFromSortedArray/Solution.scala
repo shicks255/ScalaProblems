b207f8b3c32fc0261f9057c508759f5c4e076a21
shicks <shicks255@yahoo.com>
2019-01-05 01:02:06 -0500

    working on leet code problem

---
+++ src/main/scala/LeetCodes/RemoveDuplicatesFromSortedArray/Solution.scala
@@ -0,0 +1,16 @@
+package LeetCodes.RemoveDuplicatesFromSortedArray
+
+object Solution extends App{
+
+  def removeDuplicates(nums: Array[Int]): Int = {
+
+     var set = Set()
+
+//    val removeDupe = (nums: Array[Int]) =>
+    set.size
+  }
+
+  assert(removeDuplicates(Array(1,1,2)) == 2)
+  assert(removeDuplicates(Array(0,0,1,1,1,2,2,3,3,4)) == 5)
+
+}
