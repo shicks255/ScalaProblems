4513c120346358cbc8f74fa515dae265b88cca16
shicks <shicks255@yahoo.com>
2019-02-12 10:34:20 -0500

    more monoid stuff

--- src/main/scala/Fundamentals/Monoids/MyMonoids.scala
+++ src/main/scala/Fundamentals/Monoids/MyMonoids.scala
@@ -17,4 +17,25 @@ object MyMonoids {
     override def zero: String = ""
   }
 
+  def fold[T](m: Monoid[T], l: List[T]): T = {
+    l.foldLeft(m.zero)(m.op)
+  }
+
+  def foldMap[T, Y](m: Monoid[T], l: List[Y])(f: Y => T) = {
+    l.foldLeft(m.zero){
+      case (t, y) => m.op(t, f(y))
+    }
+  }
+
+  def balancedFold[T](m: Monoid[T], l: List[T]): T = {
+    l.size match {
+      case x if x == 0 => m.zero
+      case x if x == 1 => l.head
+      case _ => {
+        val (left, right) = l.splitAt(l.length/2)
+        m.op(balancedFold(m,left), balancedFold(m, right))
+      }
+    }
+  }
+
 }
--- src/main/scala/Fundamentals/Monoids/Runner.scala
+++ src/main/scala/Fundamentals/Monoids/Runner.scala
@@ -4,6 +4,9 @@ object Runner extends App{
 
   foldExample()
   foldExampleMultiply()
+  foldAgain()
+  foldMap()
+  balancedFold()
 
   def foldExample() = {
     val nums: List[Int] = List(1,2,3,4)
@@ -14,6 +17,24 @@ object Runner extends App{
   def foldExampleMultiply() = {
     val nums: List[Int] = List(1,2,3,4)
     val result = nums.foldLeft(MyMonoids.intMultiplication.zero)(MyMonoids.intMultiplication.op)
+    println(result)
+  }
+
+  def foldAgain() = {
+    val strings = List("hi ", "wordld ", "how ", "are ", "you")
+    val result = MyMonoids.fold(MyMonoids.stringConcat, strings)
+    println(result)
+  }
+
+  def foldMap() = {
+    val strings = List("1", "2", "3", "4")
+    val result = MyMonoids.foldMap(MyMonoids.intMultiplication, strings){_.toInt}
+    println(result)
+  }
+
+  def balancedFold() = {
+    val nums = List(1,2,3,4)
+    val result = MyMonoids.balancedFold(MyMonoids.intMultiplication, nums)
     println(result)
   }
 
