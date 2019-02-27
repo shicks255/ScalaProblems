2321ace9c26fb80d4a0a826a9b4a7f7808cb4868
shicks <shicks255@yahoo.com>
2019-02-27 00:04:22 -0500

    some more list practice

--- src/main/scala/Fundamentals/FunctionalProgrammingInScala/Chapter3/List.scala
+++ src/main/scala/Fundamentals/FunctionalProgrammingInScala/Chapter3/List.scala
@@ -24,6 +24,33 @@ object List {
       Cons(a.head, apply(a.tail: _*))
   }
 
+  def tail[A](list :List[A]): List[A] = {
+    list match {
+      case Nil => Nil
+      case Cons(_,t) => t
+    }
+  }
+
+  def drop[A](n: Int, list: List[A]): List[A] = {
+      if (n > 0)
+        drop(n-1, tail(list))
+      else list
+  }
+
+  def dropWhile[A](f: A => Boolean, list: List[A]): List[A] = {
+    list match {
+      case Nil => Nil
+      case Cons(h, t) => if (f(h)) dropWhile(f,t) else Cons(h,t)
+    }
+  }
+
+  def setHead[A](a: A, list: List[A]): List[A] = {
+    list match {
+      case Nil => Cons(a, Nil)
+      case Cons(h,t) => Cons(a, t)
+    }
+  }
+
 //  def get[A](index: Int): A = {
 //
 //  }
@@ -32,20 +59,29 @@ object List {
 
 object run extends App {
 
+  val list: List[Int] = List(1,2,3,4,5)
+  println(List.tail(list))
+  println(List.drop(2, list))
+  println(List.drop(0, list))
+
+  val emptyList: List[Int] = Nil
+  println(List.tail(emptyList))
+  println(List.setHead("test", list))
+  println(List.dropWhile[Int]((a: Int) => a <= 4, list))
+
 //  val l = List(1,2,3,4)
 //
 //  println(l)
 //  println(List.sum(l))
 //  println(List.product(l))
 
-  val x = List(1,2,3,4,5) match {
-    case Cons(x, Cons(2, Cons(4, _))) => x
-    case Nil => 42
-    case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
-    case Cons(h, t) => h + List.sum(t)
-    case _ => 101
-  }
-
-  println(x)
+//  val x = List(1,2,3,4,5) match {
+//    case Cons(x, Cons(2, Cons(4, _))) => x
+//    case Nil => 42
+//    case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
+//    case Cons(h, t) => h + List.sum(t)
+//    case _ => 101
+//  }
+//  println(x)
 
 }
