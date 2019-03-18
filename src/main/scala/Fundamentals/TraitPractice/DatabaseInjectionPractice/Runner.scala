0c1c088df58da0ee5c63baf0a9eac906fd50305c
shicks <shicks255@yahoo.com>
2019-03-18 10:02:42 -0400

    dependency injection with traits

--- src/main/scala/Fundamentals/TraitPractice/DatabaseInjectionPractice/Runner.scala
+++ src/main/scala/Fundamentals/TraitPractice/DatabaseInjectionPractice/Runner.scala
@@ -1,22 +1,31 @@
 package Fundamentals.TraitPractice.DatabaseInjectionPractice
 
 object Runner extends App{
-
   val dbService: MyTestService[Int] = MyTestService[Int]
   dbService.drop(4)
   dbService.query()
   dbService.add(3)
   dbService.checkInventory
+
+  val postgresService: MyPostGresTestService[String] = MyPostGresTestService[String]
+  postgresService.drop("one")
+  postgresService.query()
+  postgresService.add("three")
+  postgresService.checkInventory
 }
 
 case class MyTestService[A]() extends Service[A]
+case class MyPostGresTestService[A]() extends PostgresService[A]
 
 trait Service[A] extends Inventory[A] with Datastore[A] {
   override val database: DatabaseImp[A] = new DatabaseImp[A]
 }
+trait PostgresService[A] extends Inventory[A] with Datastore[A] {
+  override val database: PostGresDatabaseImp[A] = new PostGresDatabaseImp[A]
+}
 
 trait Inventory[A] {
-  val database: DatabaseImp[A]
+  val database: DatabaseService[A]
 
   def checkInventory = {
     val result = database.query()
@@ -29,7 +38,7 @@ trait Inventory[A] {
 
 trait Datastore[A] {
   //dependency injection
-  val database: DatabaseImp[A]
+  val database: DatabaseService[A]
 
   def add(a: A) = database.add(a)
   def query() = database.query()
@@ -46,6 +55,18 @@ case class DatabaseImp[A]() extends DatabaseService[A] {
   }
   override def drop[A](a: A): Unit = {
     println("removing " + a + " from database")
+  }
+}
+case class PostGresDatabaseImp[A]() extends DatabaseService[A] {
+  override def add[A](a: A): Unit = {
+    println("adding to psotgres" + a)
+  }
+  override def query[A](): Option[A] = {
+    println("querying for postgres")
+    None
+  }
+  override def drop[A](a: A): Unit = {
+    println("removing " + a + " from postgres database")
   }
 }
 
