4ddc76b4c805233595891ca9d4d9bedd74a1cc63
shicks <shicks255@yahoo.com>
2019-05-28 22:00:59 -0400

    quicksort

--- src/main/scala/Fundamentals/QuickSort/QSort.scala
+++ src/main/scala/Fundamentals/QuickSort/QSort.scala
@@ -11,31 +11,33 @@ object QSort extends App{
     sort(array, 0, array.length-1)
 
     def sort(array: Array[Int], start: Integer, end: Integer): Option[Array[Int]] = {
-      if (start >= end)
-        return Some(array)
-      else {
-        val piv: Int = pivot(array, start, end)
-        sort(array, start, piv-1)
-        sort(array, piv, end)
+      (start >= end) match {
+        case true => return Some(array)
+        case false => {
+          val piv: Int = pivot(array, start, end)
+          sort(array, start, piv-1)
+          sort(array, piv, end)
+        }
       }
+
       return None
     }
 
     def pivot(array: Array[Int], start: Int, end: Int) = {
-      var counter = start
-      var endd = end
-      while (counter < endd){
-        if (array(counter) >= array(endd)) {
-          swap(array, counter, endd-1)
-          swap(array, endd, endd-1)
-          endd -= 1
+      var left = start
+      var right = end
+      while (left < right){
+        if (array(left) >= array(right)) {
+          swap(array, left, right-1)
+          swap(array, right, right-1)
+          right -= 1
         }
         else {
-          counter += 1
+          left += 1
         }
       }
 
-      endd
+      right
     }
 
     def swap(array: Array[Int], a: Int, b: Int): Unit = {
